a1bce106b2cac74ec749c477ac8c5f7e62f2712f
Irina <ira.timofeeva.2001@mail.ru>
2023-07-14 02:15:03 +0300

    Сделали условие ( 3 и больше), добавили четность и нечетность, нашли середину числа.

--- StarSquare/StarSquare/Program.cs
+++ StarSquare/StarSquare/Program.cs
@@ -4,6 +4,44 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Work");
+        string text = Console.ReadLine();
+
+        try
+        {
+            int number = Convert.ToInt32(text);
+
+            if (number >= 3)
+            {
+                Console.WriteLine("Введено правильное число");
+            }
+            else
+            {
+                Console.WriteLine("Число должно быть больше или равно 3");
+                return;
+            }
+
+            int odd = number % 2;
+
+            if (odd == 0)
+            {
+                Console.WriteLine("Введенно четное число");
+
+                int center = number / 2;
+
+                Console.WriteLine($"Середина квадрата: {center}; {center}");
+            }
+            else
+            {
+                Console.WriteLine("Введено нечетное число");
+
+                int center = number / 2;
+
+                Console.WriteLine($"Середина квадрата: {center}; {center}");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Возникла ошибка  при вводе данных.");
+        }
     }
 }
