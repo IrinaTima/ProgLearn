43ea0823c33d5cd5fec6623872cc23f5edf1cd86
Irina <ira.timofeeva.2001@mail.ru>
2023-07-15 19:51:23 +0300

    Решили палиндром.

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        string word = Console.ReadLine().ToLower();
+        string reversedWord = "";
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            reversedWord += word[i];
+        }
+        Console.WriteLine(reversedWord);
+        string outputText = (word == reversedWord)
+                       ? "Это слово палиндром"
+                       : "Это слово не палиндром";
+        Console.WriteLine(outputText);
+
+        //if (word == reversedWord)
+        //{
+        //    Console.WriteLine("Это слово палиндром");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Это слово не палиндром");
+        //}
+    }
+
+
+}
